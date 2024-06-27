7f3bb4199183c5c987e11c5d473e9432a960c04b
Bea <bcampdat@gmail.com>
2024-06-27 10:22:07 +0200

    ajustes en modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+// import { InicioModule } from './inicio/inicio.module';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +14,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // InicioModule, no es necesario porque se carga con las routes
+    HttpClientModule,
+    SharedModule
+    // PaisesModule  no se pone porque estamos usando lazyload
   ],
   providers: [],
   bootstrap: [AppComponent]
